67fd8cd2f70116ba80fd9ef471cc40ee2a1eb359
luyenhaidangit <luyenhaidangit@gmail.com>
2023-06-17 02:28:28 +0700

    Implement view layout, homepage and render main slider to home page

---
+++ resources/views/guest/layouts/head.blade.php
@@ -0,0 +1,30 @@
+<title>Bigdeal - Website kinh doanh đồ công nghệ</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="big-deal">
+    <meta name="keywords" content="big-deal">
+    <meta name="author" content="big-deal">
+    <link rel="icon" href="{{asset('guest-resource/images/favicon/favicon.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('guest-resource/images/favicon/favicon.png')}} type="image/x-icon">
+
+    <!--Google font-->
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+
+    <!--icon css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/font-awesome.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/themify.css') }}">
+
+    <!--Slick slider css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/slick-theme.css') }}">
+
+    <!--Animate css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/animate.css') }}">
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/bootstrap.css') }}">
+
+    <!-- Theme css -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('guest-resource/css/color2.css') }}" media="screen" id="color">
