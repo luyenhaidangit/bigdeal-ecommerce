67c310b63d01adb8f9d80d6fd679ad7f15042e91
luyenhaidangit <luyenhaidangit@gmail.com>
2023-06-23 14:07:47 +0700

    Implement render staff company to home page

--- resources/views/guest/home/home.blade.php
+++ resources/views/guest/home/home.blade.php
@@ -1077,4 +1077,96 @@
         </div>
     </section>
     <!--product end-->
+
+    <!--instagram start-->
+    <section class="instagram">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col p-0 position-relative">
+                    <div class="insta-contant1 ">
+                        <div class="slide-7 no-arrow">
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="instagram-box">
+                                    <img src="{{ asset('guest-resource/images/testimonial/1.jpg') }}" class="img-fluid  "
+                                        alt="insta">
+                                    <div class="insta-cover">
+                                        <i class="fa fa-instagram"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="insta-sub-contant1">
+                            <div class="insta-title">
+                                <h4><span>#</span>Thành viên công ty</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--instagra end-->
 @endsection
